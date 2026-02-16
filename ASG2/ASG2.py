d873984c90b370355cf92617f741bf6cb5bcc023
Tushar Neje <nejetushar07@gmail.com>
2026-02-16 20:48:39 +0530

    refactor(ASG2): enhance macro parameter handling
    - Add PARAMETER_TBL dictionary to store macro parameters in ASG2.py
    - Update analyze function to populate PARAMETER_TBL with formal parameters
    - Modify expand function to replace parameter references with actual values
    - Un-comment print statements for MNT, MDT, and Intermediate Code in ASG2.py

--- ASG2/ASG2.py
+++ ASG2/ASG2.py
@@ -14,7 +14,7 @@ def extract_instructions(line: list)->list:
 MNT = {}
 MDT = []
 INTERMEDIATE_CODE = []
-
+PARAMETER_TBL = {}
 def analyze(extracted_lines: list)->dict:
     macro_name = None
     macro_flag = False
@@ -25,6 +25,10 @@ def analyze(extracted_lines: list)->dict:
             macro_name = ins[1]
             MNT[macro_name] = {}
             MNT[macro_name]['params'] = len(ins[2:])
+            parameters = []
+            for param in ins[2:]:
+                parameters.append(str(param).replace(',',''))
+            PARAMETER_TBL[macro_name] = {'formal_params': parameters}
             MNT[macro_name]['mdt_index'] = len(MDT) + 1
         elif 'MEND' == ins[0]:
             MDT.append(ins)
@@ -32,34 +36,44 @@ def analyze(extracted_lines: list)->dict:
             macro_name = None
         else:
             if macro_flag:
-                MDT.append(ins)
+                params = PARAMETER_TBL[macro_name]
+                if len(ins) == 2:
+                    if ins[1] in params['formal_params']:
+                        ins[1] = f"#{params['formal_params'].index(ins[1]) + 1}"
+                        MDT.append(ins)
+                    else:
+                        MDT.append(ins)
+                else:
+                    MDT.append(ins)
             else:
                 INTERMEDIATE_CODE.append(ins)
     
-    # print("\nMNT:")
-    # print(f"{'Name of Macro':<15}|", f"{'No. of Params':<15}|", f"{'Starting Index':<15} |")
-    # print('-'*15 + '+' + '-'*16 + '+' + '-'*17 + '+')
-    # for macro in MNT.keys():
-    #     print(f"{macro:<15}|", f"{MNT[macro]['params']:<15}|", f"{MNT[macro]['mdt_index']:<15} |")
-    # print('-'*15 + '+' + '-'*16 + '+' + '-'*17 + '+')
+    print("\nMNT:")
+    print(f"{'Name of Macro':<15}|", f"{'No. of Params':<15}|", f"{'Starting Index':<15} |")
+    print('-'*15 + '+' + '-'*16 + '+' + '-'*17 + '+')
+    for macro in MNT.keys():
+        print(f"{macro:<15}|", f"{MNT[macro]['params']:<15}|", f"{MNT[macro]['mdt_index']:<15} |")
+    print('-'*15 + '+' + '-'*16 + '+' + '-'*17 + '+')
     
-    # print("\nMDT:")
-    # print(f"{'#':<15}|", f"{'MDT':<15}|")
-    # print('-'*15 + '+' + '-'*16 + '+')
-    # for code in MDT:
-    #     print(f"{MDT.index(code)+1:<15}|", f"{' '.join(code):<15}|")
+    print("\nMDT:")
+    print(f"{'#':<15}|", f"{'MDT':<15}|")
+    print('-'*15 + '+' + '-'*16 + '+')
+    for code in MDT:
+        print(f"{MDT.index(code)+1:<15}|", f"{' '.join(code):<15}|")
     
-    # print("\nIntermediate Code:")
-    # print(f"{'#':<15}|", f"{'Instrctions':<15}|")
-    # print('-'*15 + '+' + '-'*16 + '+')
-    # for code in INTERMEDIATE_CODE:
-    #     print(f"{INTERMEDIATE_CODE.index(code)+1:<15}|", f"{' '.join(code):<15}|")
+    print("\nIntermediate Code:")
+    print(f"{'#':<15}|", f"{'Instrctions':<15}|")
+    print('-'*15 + '+' + '-'*16 + '+')
+    for code in INTERMEDIATE_CODE:
+        print(f"{INTERMEDIATE_CODE.index(code)+1:<15}|", f"{' '.join(code):<15}|")
 
 def expand():
     expanded_code = []
     for code in INTERMEDIATE_CODE:
         if code[0] in MNT.keys():
             macro_name = code[0]
+            actual_params = code[1:]
+            PARAMETER_TBL[macro_name]['actual_params'] = actual_params
             mdt_index = MNT[macro_name]['mdt_index']
             macro_ins = None
             while mdt_index <= len(MDT):
@@ -68,16 +82,37 @@ def expand():
                     break
                 elif macro_ins[0] in MNT.keys():
                     nested_macro_name = macro_ins[0]
+                    nested_macro_actual_params = macro_ins[1:]
                     nested_mdt_index = MNT[nested_macro_name]['mdt_index']
                     while nested_mdt_index <= len(MDT):
                         nested_macro_ins = MDT[nested_mdt_index - 1]
                         if 'MEND' in nested_macro_ins:
                             break
                         if not nested_macro_ins[0] in MNT.keys():
+                            if nested_macro_ins[1].startswith('#'):
+                                parameter_number = int(nested_macro_ins[1][1:])
+                                if len(PARAMETER_TBL[nested_macro_name]) <= 1:
+                                    actual_params_nf = ""
+                                    for i, param in enumerate(nested_macro_actual_params):
+                                        actual_params_nf += f"{param} "
+                                    nested_macro_ins[1] = actual_params_nf
+                                else:
+                                    nested_macro_ins[1] = PARAMETER_TBL[nested_macro_name]['actual_params'][parameter_number - 1].replace(',','')
+                                
                             expanded_code.append(nested_macro_ins)
                         nested_mdt_index += 1
                 if not macro_ins[0] in MNT.keys():
-                    expanded_code.append(macro_ins)
+                            if macro_ins[1].startswith('#'):
+                                parameter_number = int(macro_ins[1][1:])
+                                if len(PARAMETER_TBL[macro_name]) <= 1:
+                                    actual_params_nf = ""
+                                    for i, param in enumerate(PARAMETER_TBL[macro_name]['actual_params']):
+                                        actual_params_nf += f"{param} "
+                                    macro_ins[1] = actual_params_nf
+                                else:
+                                    macro_ins[1] = PARAMETER_TBL[macro_name]['actual_params'][parameter_number - 1].replace(',','')
+                                
+                            expanded_code.append(macro_ins)
                 mdt_index += 1
         else:
             if not code[0] in MNT.keys():
